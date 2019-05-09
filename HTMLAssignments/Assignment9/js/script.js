8b596fde38785d3a2965be8efab8d9345b0ab807
evitajain <evita.jain157@yahoo.com>
2019-05-09 19:18:32 +0530

    from 86-151

--- HTMLAssignments/Assignment9/js/script.js
+++ HTMLAssignments/Assignment9/js/script.js
@@ -7,3 +7,11 @@
             $("#pro-service").load("pro-service.html");
             $("#footer").load("footer.html");
             }); 
+
+            $(function(){
+                  $('.navbar-nav li a').click(function(){
+                      $('.navbar-nav li a.active').removeClass('active');
+                      $(this).addClass('active');
+                  });
+              });
+              
